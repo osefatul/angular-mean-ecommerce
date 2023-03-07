c5bf145a937207cf13e6b20861df2d6c7cdbec3c
Sefatullah Omar <sefaullahomar1@gmail.com>
2023-03-06 22:10:42 -0800

    initial step is done

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,29 @@
-const dfaudslf = () => {
-    console.log("hello world");
-};
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv/config');
 
-var x = "sdfast";
+const PORT = process.env.PORT | 3000
+const api = process.env.API_URL;
+
+//Routes
+const categoriesRoutes = require('./routes/categories');
+const productsRoutes = require('./routes/products');
+const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
+
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
+
+mongoose.connect(process.env.MONGO_URL).then(
+    console.log("Connected to mongoDB")
+).catch(err => console.log(err))
+
+app.listen(PORT, ()=>{
+    console.log("server is listening on port", PORT)
+})
